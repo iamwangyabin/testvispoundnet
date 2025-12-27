ab7c05a0be9164d71c0d86d87c5c717e854ff787
Wang Yabin <38123329+iamwangyabin@users.noreply.github.com>
2025-12-27 11:20:14 +0800

    Update __init__.py

--- visualization/gradcam/__init__.py
+++ visualization/gradcam/__init__.py
@@ -9,17 +9,25 @@ from .core import ViTGradCAM
 from .poundnet_gradcam import PoundNetGradCAM
 from .utils import (
     visualize_gradcam,
+    visualize_comparative_gradcam,
+    visualize_multi_layer_analysis,
     save_gradcam_results,
     create_heatmap_overlay,
-    normalize_heatmap
+    normalize_heatmap,
+    create_attention_summary_plot,
+    preprocess_image_for_gradcam
 )
 
 __version__ = "1.0.0"
 __all__ = [
     "ViTGradCAM",
-    "PoundNetGradCAM", 
+    "PoundNetGradCAM",
     "visualize_gradcam",
+    "visualize_comparative_gradcam",
+    "visualize_multi_layer_analysis",
     "save_gradcam_results",
     "create_heatmap_overlay",
-    "normalize_heatmap"
+    "normalize_heatmap",
+    "create_attention_summary_plot",
+    "preprocess_image_for_gradcam"
 ]
